c17491bcc39af2ad4bff737d29dd32bea107b3be
Tai <letientai299@gmail.com>
2024-08-28 14:25:47 +0700

    Don't need to validate HttpErr with httpErrSchema

--- packages/server/src/utils/types.ts
+++ packages/server/src/utils/types.ts
@@ -1,14 +1,9 @@
 import express from 'express';
-import { z } from 'zod';
 
-// TODO (tai): does zod has any way to deal with this linter rule?
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const httpErrSchema = z.object({
-  err: z.object({}).optional(),
-  data: z.unknown(),
-});
-
-export type HttpErr = z.infer<typeof httpErrSchema>;
+export interface HttpErr {
+  err: Error;
+  data?: never;
+}
 
 export type IRequest<T> = express.Request<
   Record<string, string>,
